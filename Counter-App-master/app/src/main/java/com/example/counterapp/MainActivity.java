60bfa919b2332ebc35bfca008ebd09cf19de874c
HimanshuHeda <harsh967896@gmail.com>
2025-04-24 13:26:26 +0530

    Counting the Numbers

---
+++ Counter-App-master/app/src/main/java/com/example/counterapp/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.counterapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    TextView welcome_text,counter_text;
+    Button btn;
+    private int counter = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        btn = findViewById(R.id.btn);
+        counter_text = findViewById(R.id.counter_text);
+        welcome_text = findViewById(R.id.welcome_text);
+
+        //Adding the functionalities
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter_text.setText(""+increaseCounter());
+            }
+        });
+        if (savedInstanceState != null) {
+            counter = savedInstanceState.getInt("count");
+            counter_text.setText("" + counter);
+        }
+    }
+    public int increaseCounter(){
+        return ++counter;
+    }
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("count",counter);
+    }
+}
